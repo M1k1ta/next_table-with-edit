a6b3201d561b667734339391e8bc3d1de04a6004
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-10-25 23:34:14 +0300

    deploy update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
 import { readUser } from '@/utils/readUser';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,3 +1,5 @@
+'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import './PeopleTable.scss';
 import { useAppDispatch, useAppSelector } from '@/app/GlobalRedux/hooks';
